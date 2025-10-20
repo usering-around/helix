674eadd4eb8d5b37cfe8fe9ed9f74b58b1be60fb
usering <226918848+usering-around@users.noreply.github.com>
2025-10-20 08:37:29 +0300

    do not check local config if mode is set to 'always'

--- helix-term/src/config.rs
+++ helix-term/src/config.rs
@@ -75,25 +75,23 @@ impl Config {
     ) -> Result<Config, ConfigLoadError> {
         let global_config: Result<ConfigRaw, ConfigLoadError> =
             global.and_then(|file| toml::from_str(&file).map_err(ConfigLoadError::BadConfig));
-        let is_local_trusted = local.as_ref().is_ok_and(|s| {
-            trust_db::is_file_trusted(
-                &helix_loader::workspace_config_file(),
-                s.as_str().as_bytes(),
-            )
-            .is_ok_and(|b| b)
+
+        let use_local = global_config.as_ref().is_ok_and(|c| {
+            c.load_workspace_config == Some(LoadWorkspaceConfig::Always)
+                || (c.load_workspace_config.unwrap_or_default() == LoadWorkspaceConfig::Trusted
+                    && local.as_ref().is_ok_and(|s| {
+                        trust_db::is_file_trusted(
+                            &helix_loader::workspace_config_file(),
+                            s.as_str().as_bytes(),
+                        )
+                        .is_ok_and(|b| b)
+                    }))
         });
+
         let local_config: Result<ConfigRaw, ConfigLoadError> =
             local.and_then(|file| toml::from_str(&file).map_err(ConfigLoadError::BadConfig));
         let res = match (global_config, local_config) {
             (Ok(global), Ok(local)) => {
-                let use_local = if global.load_workspace_config == Some(LoadWorkspaceConfig::Always)
-                {
-                    true
-                } else if global.load_workspace_config == Some(LoadWorkspaceConfig::Never) {
-                    false
-                } else {
-                    is_local_trusted
-                };
                 let mut keys = keymap::default();
                 if let Some(global_keys) = global.keys {
                     merge_keys(&mut keys, global_keys)
